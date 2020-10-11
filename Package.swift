5b8084596bf58cdabf9303ab0093a16832bb48b3
John Bolliger <johnbolliger@gmail.com>
2020-10-11 14:53:52 -0600

    Added Package.swift with dependencies

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.0
+//
+//  Package.swift
+//  AlamofireObjectMapper
+//
+
+import PackageDescription
+
+let package = Package(name: "AlamofireObjectMapper",
+  platforms: [.iOS(.v10)],
+  products: [
+    // Products define the executables and libraries produced by a package, and make them visible to other packages.
+    .library(name: "AlamofireObjectMapper",
+           targets: ["AlamofireObjectMapper"]),
+    .library(name: "AlamofireObjectMapperDynamic", type: .dynamic, targets: ["AlamofireObjectMapper"]),
+  ],
+  dependencies: [
+    .package(url: "https://github.com/Alamofire/Alamofire", .upToNextMajor(from: Version(6,0,0))),
+    .package(url: "https://github.com/tristanhimmelman/ObjectMapper", .upToNextMajor(from: Version(4,0,0))),
+  ],
+  targets: [
+    .target(name: "AlamofireObjectMapper",
+            dependencies: ["Alamofire", "ObjectMapper"],
+            path: "AlamofireObjectMapper"),
+  ]
+)
