661829f5f352152619dbe7e8c589551f44a02e51
John Bolliger <johnbolliger@gmail.com>
2020-10-11 15:12:03 -0600

    removed dynamic target

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(name: "AlamofireObjectMapper",
     // Products define the executables and libraries produced by a package, and make them visible to other packages.
     .library(name: "AlamofireObjectMapper",
            targets: ["AlamofireObjectMapper"]),
-    .library(name: "AlamofireObjectMapperDynamic", type: .dynamic, targets: ["AlamofireObjectMapper"]),
+    // .library(name: "AlamofireObjectMapperDynamic", type: .dynamic, targets: ["AlamofireObjectMapper"]),
   ],
   dependencies: [
     .package(url: "https://github.com/Alamofire/Alamofire", .upToNextMajor(from: Version(5,0,0))),
